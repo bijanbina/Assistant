5f30f900dc9869e86f486a893302a24a220d0aa6
bijanbina <bijanbina@gmail.com>
2016-03-30 17:12:35 +0430

    Critical: fix PH Add url

--- Scripts/ph_add.sh
+++ Scripts/ph_add.sh
@@ -20,4 +20,4 @@ fi
 
 url_add=$( gsettings get org.binaee.assistant url-code |  awk -F "'" '{printf $2}' ) #get url code *(update automatically in ph_download.sh)
 
-wget  -qO- --show-progress -U 'Mozilla/5.0'  --header="Cookie: $cookie" --post-data="q=$WORD&utrans=$TRANSLATE" $url_add #get phrasebook from google
+wget  -qO- --show-progress -U 'Mozilla/5.0'  --header="Cookie: $cookie" --post-data="q=$WORD&utrans=$TRANSLATE" "https://translate.google.com/translate_a/sg?client=t&cm=a&sl=en&tl=fa&ql=2&hl=en&xt=$url_add" #get phrasebook from google
