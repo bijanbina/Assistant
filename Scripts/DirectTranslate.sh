70bca41264326b47f41dd8e8810a36eeb0b253cb
bijanbina <bijanbina@gmail.com>
2016-08-04 22:51:56 +0430

    faster direct translate

--- Scripts/DirectTranslate.sh
+++ Scripts/DirectTranslate.sh
@@ -2,9 +2,7 @@ assistant_pid=$( ps -A | grep -i assistant )
 
 if [ -n "$assistant_pid" ]; then
 	echo "good"
+	dbus-send --session --dest=com.binaee.assistant  / com.binaee.assistant.direct
 else
-	/home/bijan/Project/Assistant/Sources/Assistant &
-	sleep 0.5
+	/home/bijan/Project/Assistant/Sources/Assistant --direct&
 fi
-
-dbus-send --session --dest=com.binaee.assistant  / com.binaee.assistant.direct
--- Sources/main.cpp
+++ Sources/main.cpp
@@ -4,7 +4,34 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-
+    bool loadDirect = false;
+    if (argc > 1)
+    {
+        if (strcmp (argv[1],"--help") == 0)
+        {
+            if (argc == 2)
+            {
+                printf("Usage:\n  assistant [OPTION...]\n\nApplication Options:\n  --help\t\t\t  Show help options\n  --direct\t\t\t  Direct translation on first load\n\nShatter Me\n");
+            }
+            else
+            {
+               perror("you use this software in wrong syntax please type \"--help\" for more information");
+               return 2;
+            }
+        }
+        if (strcmp (argv[1],"--direct") == 0)
+        {
+            if (argc == 2)
+            {
+                loadDirect = true;
+            }
+            else
+            {
+               perror("you use this software in wrong syntax please type \"--help\" for more information");
+               return 2;
+            }
+        }
+    }
     //Added for GUI version
 
     QQmlApplicationEngine engine;
@@ -19,6 +46,11 @@ int main(int argc, char *argv[])
 
     loadOptions();
     dbusChnl->startServer();
+
+    if (loadDirect)
+    {
+        dbusChnl->translateDirect();
+    }
 
     return app.exec();
 }
