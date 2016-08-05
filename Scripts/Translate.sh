3c9357383502f1c2ffc91ac1638ff20fdbc9c18a
bijanbina <bijanbina@gmail.com>
2016-08-05 23:51:28 +0430

    remove any sleep in translation

--- Scripts/Translate.sh
+++ Scripts/Translate.sh
@@ -1,19 +1,19 @@
 assistant_pid=$( ps -A | grep -i assistant )
 
-if [ -n "$assistant_pid" ]; then
-	echo "good"
-else
-	/home/bijan/Project/Assistant/Sources/Assistant &
-	sleep 0.3
-fi
-
-
 if [ -n "$1" ]; then
 	word=$1
 else
 	word=$( xsel -o | sed "s/[\"'<>]//g" )
 fi
 
-#this use calling directly the function not calling the signal
-#dbus-send --session --dest=com.binaee.assistant --type=method_call  / com.binaee.assistant.translate string:"$word"  
-dbus-send --session --dest=com.binaee.assistant / com.binaee.assistant.translate string:"$word"
+if [ -n "$assistant_pid" ]; then
+	#this use calling directly the function not calling the signal
+	#dbus-send --session --dest=com.binaee.assistant --type=method_call  / com.binaee.assistant.translate string:"$word"  
+	dbus-send --session --dest=com.binaee.assistant / com.binaee.assistant.translate string:"$word"
+else
+	/home/bijan/Project/Assistant/Sources/Assistant --translate "$word" &
+fi
+
+
+
+
--- Sources/main.cpp
+++ Sources/main.cpp
@@ -5,13 +5,16 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     bool loadDirect = false;
+    bool loadTranslate = false;
     if (argc > 1)
     {
         if (strcmp (argv[1],"--help") == 0)
         {
             if (argc == 2)
             {
-                printf("Usage:\n  assistant [OPTION...]\n\nApplication Options:\n  --help\t\t\t  Show help options\n  --direct\t\t\t  Direct translation on first load\n\nShatter Me\n");
+                printf("Usage:\n  assistant [OPTION...]\n\nApplication Options:\n  --help\t\t\t  Show help options\n  --direct\t\t\t  Direct translation on first load"
+                       "\n  --translate word\t\t\t  translate word on first load\n\nShatter Me\n");
+                return 0;
             }
             else
             {
@@ -24,6 +27,18 @@ int main(int argc, char *argv[])
             if (argc == 2)
             {
                 loadDirect = true;
+            }
+            else
+            {
+               perror("you use this software in wrong syntax please type \"--help\" for more information");
+               return 2;
+            }
+        }
+        if (strcmp (argv[1],"--translate") == 0)
+        {
+            if (argc == 3)
+            {
+                loadTranslate = true;
             }
             else
             {
@@ -50,6 +65,10 @@ int main(int argc, char *argv[])
     if (loadDirect)
     {
         dbusChnl->translateDirect();
+    }
+    else if (loadTranslate)
+    {
+        dbusChnl->translate(argv[2]);
     }
 
     return app.exec();
